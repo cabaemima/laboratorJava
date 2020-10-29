d40bac03d45b8f2699ea822124cdb284afdb8f8f
cabaemima <caba.emimaa@gmail.com>
2020-10-29 15:24:10 +0200

    simple Maps exercises

--- src/CollenctionsPractice.java
+++ src/CollenctionsPractice.java
@@ -1,19 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollenctionsPractice {
     public static void main(String[] args) {
         List<City> cities = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Number of cities: ");
-        int n = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            cities.add(new City(scanner.next(), scanner.nextInt()));
-        }
-        System.out.println(cities.stream()
-                .filter(city -> city.getPopulation() > 10000)
-                .count());
+//        System.out.println("Number of cities: ");
+//        int n = scanner.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            cities.add(new City(scanner.next(), scanner.nextInt()));
+//        }
+//        System.out.println(cities.stream()
+//                .filter(city -> city.getPopulation() > 10000)
+//                .count());
+
+        Map<Integer,City> map = new HashMap<>();
+        map.put(1,new City("Oradea", 12345));
+        map.put(2,new City("Cluj", 12345));
+        map.put(3,new City("Brasov", 12345));
+
+        System.out.println(map.get(2).getName());
+        System.out.println(map.containsKey(4));
+        System.out.println(map.keySet());
+        System.out.println(map.isEmpty());
+        map.remove(2);
+        System.out.println(map.get(2));
     }
 }
